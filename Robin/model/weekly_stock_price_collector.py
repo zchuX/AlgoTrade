3a127e8a56dbf92e1174150f6e10da445068b3ad
Macbook Sill <meow@Macbooks-MacBook-Pro.local>
2023-10-15 23:13:43 -0700

    2023-10-15 data

--- Robin/model/weekly_stock_price_collector.py
+++ Robin/model/weekly_stock_price_collector.py
@@ -4,4 +4,4 @@ symbols = ["TLSA", "AAPL", "MSFT", "NVDA", "META"]
 
 for symbol in symbols:
 	stock_prices = yf.download(tickers=symbol, period="5d", interval="1m")
-	stock_prices.to_csv(f"2023-09-17-{symbol}.csv")
+	stock_prices.to_csv(f"2023-10-15-{symbol}.csv")
